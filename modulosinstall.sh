17156651fa7ec8cabf5b2869acb4f2e3e97e1af4
DuiBR <95405352+DuiBR@users.noreply.github.com>
2025-07-15 15:54:21 -0300

    Add files via upload

--- modulosinstall.sh
+++ modulosinstall.sh
@@ -1,39 +1,24 @@
 #!/bin/bash
-set -e
 export PATH=$PATH:/usr/sbin
-
-# 1) Desativa IPv6 temporariamente
-sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
-sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
-
-# 2) Detecta arquitetura e baixa o jq do repositório modulosTeste na raiz (/root)
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
 cake=$(uname -m)
-sudo wget -qO /root/jq "https://raw.githubusercontent.com/DuiBR/modulosTeste/main/${cake}"
-sudo chmod +x /root/jq
-
-# 3) Remove módulos antigos na raiz
-sudo rm -f /root/atlasdata.sh /root/atlascreate.sh /root/atlasteste.sh /root/atlasremove.sh \
-      /root/delete.py /root/sincronizar.py /root/add.sh /root/rem.sh /root/addteste.sh /root/addsinc.sh /root/remsinc.sh /root/jq /root/verificador.py
-
-# 4) Baixa os scripts do modulosTeste (mesmos links do Script 1) na raiz
-sudo wget -qO /root/sshplus.sh     "https://raw.githubusercontent.com/DuiBR/modulosTeste/main/sshplus.sh"
-sudo wget -qO /root/dragonmodule   "https://raw.githubusercontent.com/DuiBR/modulosTeste/main/dragon.sh"
-sudo wget -qO /root/delete.py      "https://raw.githubusercontent.com/DuiBR/modulosTeste/main/delete.py"
-sudo wget -qO /root/sincronizar.py "https://raw.githubusercontent.com/DuiBR/modulosTeste/main/sincronizar.py"
-sudo wget -qO /root/verificador.py "https://raw.githubusercontent.com/DuiBR/modulosTeste/main/verificador.py"
-
-# 5) Torna executáveis
-sudo chmod +x /root/sshplus.sh /root/dragonmodule /root/jq /root/verificador.py /root/delete.py /root/sincronizar.py
-
-# 6) Instala dos2unix (caso não exista)
-if ! command -v dos2unix &>/dev/null; then
-  sudo apt-get update -y
-  sudo apt-get install dos2unix -y
+wget  "https://module.dragoncoressh.com/modulos/${cake}" -O jq
+chmod +x jq
+if [ -d "/opt/DragonCore/" ]; then
+rm -f atlasdata.sh atlascreate.sh atlasteste.sh atlasremove.sh delete.py sincronizar.py add.sh rem.sh addteste.sh addsinc.sh remsinc.sh
+wget  "https://module.dragoncoressh.com/modulod2/dragon.sh" -O dragonmodule
+wget  "https://module.dragoncoressh.com/modulod2/delete.py" -O delete.py
+wget  "https://module.dragoncoressh.com/modulod2/sincronizar.py" -O sincronizar.py
+chmod 777 dragonmodule delete.py sincronizar.py
+else
+rm -f atlasdata.sh atlascreate.sh atlasteste.sh atlasremove.sh delete.py sincronizar.py add.sh rem.sh addteste.sh addsinc.sh remsinc.sh
+wget  "https://module.dragoncoressh.com/modulod2/sshplus.sh" -O dragonmodule
+wget  "https://module.dragoncoressh.com/modulod2/delete.py" -O delete.py
+wget  "https://module.dragoncoressh.com/modulod2/sincronizar.py" -O sincronizar.py
+chmod 777 dragonmodule delete.py sincronizar.py
 fi
-
-# 7) Executa o verificador (rodando em /root)
-sudo python3 /root/verificador.py
-
-# 8) Restaura IPv6
-sudo sysctl -w net.ipv6.conf.all.disable_ipv6=0
-sudo sysctl -w net.ipv6.conf.default.disable_ipv6=0
+wget "https://module.dragoncoressh.com/modulos/verificador.py" -O verificador.py 
+python3 verificador.py
+sysctl -w net.ipv6.conf.all.disable_ipv6=0
+sysctl -w net.ipv6.conf.default.disable_ipv6=0
